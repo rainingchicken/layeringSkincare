4ea50a0422bf4b129da8d092a3b80953a7870820
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2023-12-28 07:11:42 -0800

    Update dataanalysis.py

--- dataanalysis.py
+++ dataanalysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('skincare_products_clean.csv')
+url = 'https://raw.githubusercontent.com/rainingchicken/LayeringSkincare/blob/main/skincare_products_clean.csv'
+
+df = pd.read_csv(url, index_col=0)
 
 print(df.head())
