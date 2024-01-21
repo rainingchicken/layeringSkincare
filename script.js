38670c98eaad53ee64acde9883aea6e8bcc07c56
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-01-20 18:27:39 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var n = 1; //the nth number of div
+var n = 0; //the nth number of div
 //dynamically create input boxes
 function addElement() {
   n++; //increments n as new elements are created
@@ -14,6 +14,24 @@ function addElement() {
   newinput.setAttribute("name", "inputbox"); //sets name="inputbox" very important!
   newinput.onblur = autocomplete(newinput, product); //applys autocomplete function to current created input
 
+  //create collapsible element that shows ingredient list
+  const ingredientbox = document.createElement("button");
+  ingredientbox.setAttribute("id", "" + n + "");
+  ingredientbox.setAttribute("type", "button");
+  ingredientbox.setAttribute("class", "collapsebox");
+  ingredientbox.setAttribute("onclick", "collapse(this)");
+  ingredientbox.setAttribute(
+    "title",
+    "Click to show or hide ingredients of product"
+  );
+  ingredientbox.textContent = "SHOW/HIDE INGREDIENTS";
+
+  //create div that shows when pressed button
+  const collapsediv = document.createElement("div");
+  collapsediv.setAttribute("class", "collapse");
+  collapsediv.setAttribute("id", "collapsediv" + n);
+  collapsediv.innerHTML = "List of ingredients will be shown here";
+
   //create delete button to remove input and div
   const deletebtn = document.createElement("button");
   deletebtn.setAttribute("id", "" + n + "");
@@ -21,10 +39,13 @@ function addElement() {
   deletebtn.textContent = "DELETE";
   deletebtn.setAttribute("type", "button");
   deletebtn.setAttribute("onclick", "remove(this)"); //when click delete div and everything in it
+  deletebtn.setAttribute("title", "Delete product");
 
   //put created elements in newdiv so they are grouped together and will easily deleted
   newdiv.appendChild(newinput);
   newdiv.appendChild(deletebtn);
+  newdiv.appendChild(ingredientbox);
+  newdiv.appendChild(collapsediv);
 
   document.getElementById("div1").appendChild(newdiv); //places created group in div1
 }
@@ -36,6 +57,18 @@ function remove(button) {
   ddiv.remove(); //specificelement.remove()
 }
 
+//function to hide and unhide ingredient list
+function collapse(button) {
+  let n = button.id;
+  let collapsediv = document.getElementById("collapsediv" + n);
+  //toggles display on and off to show and hide collapsediv
+  if (collapsediv.style.display === "block") {
+    collapsediv.style.display = "none";
+  } else {
+    collapsediv.style.display = "block";
+  }
+}
+
 //starts with at least two products to compare
 addElement();
 addElement();
@@ -43,7 +76,7 @@ addElement();
 //////////////////////////////////////////////////
 //autocomplete
 //https://www.educative.io/answers/how-to-add-autocomplete-to-an-input-field-in-javascript
-
+//parameters are input elements and array of autocomplete suggestions
 function autocomplete(input, list) {
   //Add an event listener to compare the input value with all countries
   input.addEventListener("input", function () {
@@ -64,7 +97,9 @@ function autocomplete(input, list) {
         //If a match is found, create a suggestion <div> and add it to the suggestions <div>
         suggestion = document.createElement("div");
         suggestion.innerHTML = list[i];
-        suggestion.style.padding = "5px";
+        suggestion.style.padding = "7px";
+        suggestion.style.border = "solid";
+        suggestion.style.borderWidth = "thin";
 
         suggestion.addEventListener("click", function () {
           input.value = this.innerHTML;
@@ -99,9 +134,6 @@ function submitcheck() {
     inputArr.push(arrayelement); //put strings into array
   });
 
-  //since form gets reset where everything is earsed inputs will be stored in id="inputs"
-  document.getElementById("inputs").innerHTML = inputArr;
-
   var indexes = []; //to get ingredient at index
   inputArr.forEach((arrElement) => {
     //create collapsible div that to show and hide ingredient list
@@ -114,13 +146,24 @@ function submitcheck() {
       }
     });
   });
-
+  var itemnum = 0; //counts number of product items
   var layer = []; //array of ingredient list
   indexes.forEach((i) => {
+    itemnum++;
     let ingredientlist = ingredient[i];
+    var showningredient = document.getElementById("collapsediv" + itemnum);
+    //restart to first itemnum and go through each element again to combat skipping id from deletion or multiple addition of inputbox
+    for (let j = 0; j < itemnum; j++) {
+      if (showningredient == null) {
+        //keep incrementing itemnum until find an existing collapse div
+        itemnum++;
+        showningredient = document.getElementById("collapsediv" + itemnum);
+      }
+    }
+    showningredient.innerHTML = ingredientlist; //set ingredient to text of collapsediv
     layer.push(ingredientlist);
   });
-  console.log(layer);
+
   //flags for each ingredients that one should take caution when using
   var retinol = false;
   var vitc = false;
@@ -130,10 +173,11 @@ function submitcheck() {
   var bha = 0;
   var niacinamide = 0;
   var hydroquinone = 0;
+
   layer.forEach((element) => {
     if (
       element.includes("retinol") ||
-      element.includes("retinyl palmitate ") ||
+      element.includes("retinyl palmitate") ||
       element.includes("retinyl acetate") ||
       element.includes("retinyl linoleate")
     ) {
@@ -144,11 +188,11 @@ function submitcheck() {
       element.includes("vitamin c") ||
       element.includes("ascorbic acid") ||
       element.includes("ascorbyl glucoside") ||
-      element.includes("vitamin C glucoside") ||
-      element.includes("vitamin CG")
+      element.includes("vitamin c glucoside") ||
+      element.includes("vitamin cg")
     ) {
       vitc = true;
-    } else if (element.includes(" salicylic acid")) {
+    } else if (element.includes("salicylic acid")) {
       salicylicacid = true;
     } else if (
       element.includes("lactic acid") ||
@@ -171,7 +215,7 @@ function submitcheck() {
       element.includes("nicotinamide")
     ) {
       niacinamide = true;
-    } else if (element.includes("hydroquinone ")) {
+    } else if (element.includes("hydroquinone")) {
       hydroquinone = true;
     }
   });
@@ -227,12 +271,73 @@ function submitcheck() {
   if (warning.length > 0) {
     document.getElementById("result").innerHTML =
       "Not recommended! Products contain combinations of" + warning;
+    //if products are not recommended together show ingredients to help user choose which to keep
+    for (
+      let ingredientbuttons = 0;
+      ingredientbuttons < document.getElementsByClassName("collapsebox").length;
+      ingredientbuttons++
+    ) {
+      document.getElementsByClassName("collapse")[
+        ingredientbuttons
+      ].style.display = "block";
+    }
   } else {
     document.getElementById("result").innerHTML = "Everything looks good!";
+    //if ingredients dont contradict each other then hide ingredient list
+    for (
+      let ingredientbuttons = 0;
+      ingredientbuttons < document.getElementsByClassName("collapsebox").length;
+      ingredientbuttons++
+    ) {
+      document.getElementsByClassName("collapse")[
+        ingredientbuttons
+      ].style.display = "none";
+    }
+  }
+
+  //array of active ingredients that tend to not mix well together
+  var keyingredient = [
+    "retinol",
+    "retinyl palmitate",
+    "retinyl acetate",
+    "retinyl linoleate",
+    "benzoyl peroxide",
+    "vitamin c",
+    "ascorbic acid",
+    "ascorbyl glucoside",
+    "vitamin c glucoside",
+    "vitamin cg",
+    "salicylic acid",
+    "lactic acid",
+    "glycolic acid",
+    "malic acid",
+    "citric acid",
+    "tartaric acid",
+    "beta hydroxybutanoic acid",
+    "tropic acid",
+    "trethocanic acid",
+    "niacinamide",
+    "vitamin b3",
+    "nicotinamide",
+    "hydroquinone",
+  ];
+
+  //count number of items in page and go through each one to highlight the problematic ingredients
+  var itemnum2;
+  for (itemnum2 = 1; itemnum2 <= itemnum; itemnum2++) {
+    let elementboxes = document.getElementById("collapsediv" + itemnum2);
+    if (elementboxes != null) {
+      for (let i = 0; i < keyingredient.length; i++) {
+        elementboxes.innerHTML = elementboxes.innerHTML.replaceAll(
+          keyingredient[i],
+          "<span style='color:#e0829d;'>" + keyingredient[i] + "</span>"
+        );
+      }
+    }
   }
 
   //reset when click submit
-  document.getElementById("myform").reload();
+
   retinol = false;
   vitc = false;
   benzoylperoxide = 0;
@@ -241,6 +346,9 @@ function submitcheck() {
   bha = 0;
   niacinamide = 0;
   hydroquinone = 0;
+  itemnum = 0;
+  itemnum2 = 1;
+  indexes = [];
 }
 
 // Get the button:
